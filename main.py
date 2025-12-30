fb88251d8ba51aa33522982d9938e7fee22c5040
LGZ <liguangzheng998@hotmail.com>
2025-12-30 15:00:05 +0800

    work on pkm model

--- main.py
+++ main.py
@@ -4,52 +4,10 @@ from json import loads
 from json.decoder import JSONDecodeError
 from typing import Dict
 import traceback
+from models.Pokemon import Pokemon
 
 
 PKM_LIST_DIR = "./resources/pkm.json"
-
-def format_pkm_info(j: Dict[str, Any]) -> str:
-    ## format
-    """
-    num
-    name
-    types
-    male_ratio
-    hp
-    atk
-    def
-    spa
-    spd
-    spe
-    abilities
-    heightm
-    weightkg
-    eggGroups
-    """
-    try:
-        stats = j["baseStats"]
-        return f"index num: {j["num"]}\t" +\
-                f"name: {j["name"]}\t" +\
-                f"types: {','.join(j["types"])}\t" +\
-                f"male ratio: {get_male_ratio_from_json(j)}\n" +\
-                f"HP: {stats["hp"]} ATK: {stats["atk"]}" +\
-                f" DEF: {stats["def"]} SpA: {stats["spa"]}" +\
-                f" SpD: {stats["spd"]} Spe: {stats["spe"]}\n" +\
-                f"abilities: {','.join([j["abilities"][k] for k in j["abilities"] if k != 'H'])}\t" +\
-                f"height: {j["heightm"]}m\tweight: {j["weightkg"]}kg\n" +\
-                f"color: {j["color"]}\n" +\
-                f"egg groups: {','.join(j["eggGroups"])}"
-    except Exception as e:
-        print(f"{j["num"]} {j["name"]} missing field")
-        # print(traceback.format_exc())
-        
-def get_male_ratio_from_json(j: Dict) -> float:
-    if "genderRatio" in j:
-        return j["genderRatio"]["M"]
-    elif "gender" in j and j["gender"] == 'N':
-        return -1.0 ## metang
-    elif "gender" not in j and "genderRatio" not in j:
-        return -1.0
 
 
 def main():
@@ -67,7 +25,17 @@ def main():
     for pk_name in pkm_dict:
         try:
             if "num" in pkm_dict[pk_name] and 0 < pkm_dict[pk_name]["num"] <=386:
-                print(format_pkm_info(pkm_dict[pk_name]))
+                # print(
+                #     Pokemon(
+                #         j["num"], j["name"], j["types"],
+                #         get_male_ratio_from_json(j),
+                #         j["baseStats"],
+                #         [j["abilities"][k] for k in j["abilities"] if k != 'H'],
+                #         j["heightm"], j["weightkg"],
+                #         j["color"], j["eggGroups"]
+                #     )
+                # )
+                print(Pokemon.from_json(pkm_dict[pk_name]))
         except KeyError as ke:
             print(f"{pk_name} no ...")
 
---
+++ models/Pokemon.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+from typing import List
+
+class Pokemon:
+    def __init__(
+        self, num: int, name: str, types: List[str],
+        male_ratio: float, baseStats: Dict[str, int],
+        abilities: List[str], height: int, weight: int,
+        color: str, egg_groups: List[str]
+    ):
+        self.__index_num: int = num
+        self.__name: str = name
+        self.__types: List[str] = types
+        self.__male_ratio: float = male_ratio
+        self.__hp: int = baseStats["hp"]
+        self.__atk: int = baseStats["atk"]
+        self.__def: int = baseStats["def"]
+        self.__spa: int = baseStats["spa"]
+        self.__spd: int = baseStats["spd"]
+        self.__spe: int = baseStats["spe"]
+        self.__abilities = abilities
+        self.__height: int = height
+        self.__weight: int = weight
+        self.__color: str = color
+        self.__egg_groups: List[str] = egg_groups
+
+    @classmethod
+    def from_json(cls, j: Dict):
+        """
+        create Pokemon from json
+        """
+        return cls(
+            j["num"], j["name"], j["types"],
+            cls.get_male_ratio_from_json(j),
+            j["baseStats"],
+            [j["abilities"][k] for k in j["abilities"] if k != 'H'],
+            j["heightm"], j["weightkg"],
+            j["color"], j["eggGroups"]
+        )
+
+
+    def __str__(self) -> str:
+        return f"{self.__index_num} {self.__name}\n" +\
+                f"types: {self.__types}\n" +\
+                f"egg groups: {self.__egg_groups}\n" +\
+                f"HP: {self.__hp} ATK: {self.__atk}" +\
+                f" DEF: {self.__def} SpA: {self.__spa}" +\
+                f" SpD: {self.__spd} Spe: {self.__spe}\t" +\
+                f"abilities: {self.__abilities}\n" +\
+                f"height: {self.__height}m\t" +\
+                f"weight: {self.__weight}kg\t" +\
+                f"color: {self.__color}\t" + f"male ratio: {self.__male_ratio}"
+
+    @staticmethod
+    def get_male_ratio_from_json(j: Dict) -> float:
+        if "genderRatio" in j:
+            return j["genderRatio"]["M"]
+        elif "gender" in j and j["gender"] == 'N':
+            return -1.0 ## e.g. metang
+        elif "gender" not in j and "genderRatio" not in j:
+            return -1.0
